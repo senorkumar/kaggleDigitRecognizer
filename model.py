f5b391964efe67815e425b489e7e5d699ae783c1
senorkumar <sdkumar@cs.hmc.edu>
2019-06-09 15:01:58 -0600

    added first layer

--- model.py
+++ model.py
@@ -1,17 +1,24 @@
-import tensorflow as tf
-from tensorflow import keras
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
 
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import sklearn
+from sklearn.model_selection import train_test_split
+
+
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
-#think of mapping Y = f(X), so X is the images and Y is the labels
+#think of mapping Y = f(X), so X is the images (28x28 pixels) and Y is the labels
 
 #mapping input number to correct label
 Y_train = train["label"]
@@ -29,5 +36,10 @@ test = test.values.reshape(-1,28,28,1)
 #one hot encode Y train labels
 Y_train = to_categorical(Y_train, num_classes = 10)
 
+#create testing and validation set
 random_seed = 2
 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
+
+#create the model!
+model  = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = 2, padding = 'Same', data_format = 'channels_last', activation = 'relu'))
